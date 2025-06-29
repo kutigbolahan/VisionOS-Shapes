47c4c44ad5c63204b765fde5427c9257d211f536
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-06-29 12:48:27 +0100

    Added triangle to the contentview

--- shapes/ContentView.swift
+++ shapes/ContentView.swift
@@ -16,9 +16,14 @@ struct ContentView: View {
         let shapeScale: CGFloat = 100.0
         let strokeWidth: CGFloat = 5.0
         
+        let vertex1 = CGPoint(x:0, y:0)
+        let vertex2 = CGPoint(x:shapeScale, y: shapeScale)
+        let vertex3 = CGPoint(x:0.0, y: shapeScale)
+        
         HStack(spacing: spaceBetweenShapes){
             Circle().stroke(lineWidth: strokeWidth).frame(width:shapeScale, height: shapeScale)
             Rectangle().stroke(lineWidth: strokeWidth).frame(width: shapeScale, height: shapeScale)
+            Triangle(vertex1: vertex1, vertex2: vertex2, vertex3: vertex3).stroke(lineWidth: strokeWidth).frame(width:shapeScale, height: shapeScale)
         }
         
         
--- shapes/Triangle.swift
+++ shapes/Triangle.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUICore
+
+
+struct Triangle: Shape{
+    let vertex1: CGPoint
+    let vertex2: CGPoint
+    let vertex3: CGPoint
+    
+    func path(in bounds: CGRect) -> Path {
+        var path = Path()
+        path.move(to: vertex1)
+        path.addLine(to: vertex2)
+        path.addLine(to: vertex3)
+        path.closeSubpath()
+        
+        return path
+    }
+}
